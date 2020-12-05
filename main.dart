b4e5757fa57ede33f4c99b9648b178806cf24a1f
Mubeen Rao <74795094+Mubeenrao015@users.noreply.github.com>
2020-12-05 03:25:28 -0800

    Add files via upload

--- main.dart
+++ main.dart
@@ -1,62 +1,22 @@
 import 'package:flutter/material.dart';
-import 'package:rolladice/ui/home_view.dart';
-import 'package:rolladice/service_locator.dart';
-import 'package:rolladice/scoped_models/settings_model.dart';
-import 'package:scoped_model/scoped_model.dart';
-import 'package:rolladice/enums/view_states.dart';
+import 'package:rolling_Dice/homescreen.dart';
+import 'package:rolling_Dice/splash.dart';
+
 
 void main() {
-  setupLocator();
-  locator<SettingsModel>().getSavedSettings();
-  runApp(MyApp());
+  runApp(
+    MyApp(),
+  );
 }
 
 class MyApp extends StatelessWidget {
-
-  ThemeData _setTheme(bool isDark) {
-    if(!isDark) {
-      return ThemeData(
-        primaryColor: Colors.deepOrange,
-        brightness: Brightness.light,
-        accentColor: Colors.deepOrangeAccent,
-        floatingActionButtonTheme: FloatingActionButtonThemeData(
-          foregroundColor: Colors.white,
-        )
-      );
-    } else {
-      return ThemeData(
-        primaryColor: Colors.deepOrange,
-        brightness: Brightness.dark,
-        accentColor: Colors.deepOrangeAccent,
-        floatingActionButtonTheme: FloatingActionButtonThemeData(
-          foregroundColor: Colors.black,
-        )
-      );
-    }
-  }
+  const MyApp({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return ScopedModel<SettingsModel>(
-      model: locator<SettingsModel>(),
-      child: ScopedModelDescendant<SettingsModel>(
-        builder: (context, child, model) {
-          
-          switch (model.state) {
-            case ViewState.Retreived:
-              return MaterialApp(
-                title: 'Dice Roll',
-                theme: _setTheme(model.isDark),
-                home:HomeView(),
-              );
-              break;
-            default: 
-              return Center(child: CircularProgressIndicator(),);
-          }
-        }
-      )    
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
     );
   }
-
-
 }
